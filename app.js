5aee26cc334bb29d5bfae1e5c58a01c31f8a4d7c
Nbred <thehardyboiz1@gmail.com>
2024-12-08 13:25:21 -0500

    Weird spiral, not DFT, unexpected result

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const canvas = document.getElementById('canvas');
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
+canvas.style.setProperty("--scaling", 1)
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -11,28 +12,72 @@ const HEIGHT = window.innerHeight;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
-ctx.translate(WIDTH/2, HEIGHT/2)
+
+// Canvas Axis and Origin Point
+
+ctx.translate(WIDTH / 2, HEIGHT / 2)
 ctx.scale(1, -1);
 ctx.fillStyle = 'white'
 
-
 const axisLineWidth = 1;
 
-ctx.fillRect(0,-HEIGHT/2, axisLineWidth, HEIGHT)
-ctx.fillRect(-WIDTH/2, 0, WIDTH, axisLineWidth)
-
-
-ctx.fillRect(0,0,10,10)
+ctx.fillRect(0, -HEIGHT / 2, axisLineWidth, HEIGHT)
+ctx.fillRect(-WIDTH / 2, 0, WIDTH, axisLineWidth)
 ctx.stroke()
+
 
 
 
 // DFT
 
 
-const maxn = 100;
+
+const complex = (re, im) => {
+    return {
+        re,
+        im,
+        multiply: (re2, im2) => {
+            return complex((re * re2 - im * im2), (re * im2 + im + re2) * -1);
+        }
+    }
+}
+
+const maxn = 500;
 
 const signal = []
 for (let i = 0; i < maxn; ++i) {
-    signal.push([i, Math.sin(i)])
+    signal.push(complex(i, Math.sin(i)))
 }
+
+const dft = (signal) => {
+    // 1/N sum(0, N-1): Fn * e^-i2Pink/N
+    const N = signal.length;
+    const normalizer = 1 / N;
+    const fk = []
+    for (let k = 0; k < 10; ++k) {
+        for (let n = 0; n < N; ++n) {
+            const c = signal[n];
+            const exp = (2 * Math.PI * n * k) / N;
+            const e = complex(Math.cos(exp), -Math.sin(exp));
+            const result = c.multiply(e.re, e.im);
+            fk.push(result);
+        }
+    }
+
+    return fk
+}
+
+
+// Canvas Function drawer
+
+const squareSize = 1;
+
+signal.forEach(c => {
+    // ctx.fillRect(c.re * squareSize, c.im * squareSize, squareSize, squareSize)
+})
+
+const values = dft(signal);
+
+values.forEach(c => {
+    ctx.fillRect(c.re * squareSize, c.im * squareSize, squareSize, squareSize)
+})
